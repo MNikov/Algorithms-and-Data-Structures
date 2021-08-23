951b2c72e22b07b927a41374b2438db76c6b9c17
Momchil Nikov <momchildnikov@gmail.com>
2021-08-24 01:16:34 +0300

    Added a recursive method to one of the questions regarding stack and queues

--- Stacks/Interview_Questions/queue_with_stack.py
+++ Stacks/Interview_Questions/queue_with_stack.py
@@ -19,3 +19,21 @@ class Queue:
                 self.dequeue_stack.append(self.enqueue_stack.pop())
 
         return self.dequeue_stack.pop()
+
+
+# Using recursion
+class QueueR:
+    def __init__(self):
+        self.stack = []
+
+    def enqueue(self, data):
+        self.stack.append(data)
+
+    def dequeue(self):
+        if len(self.stack) == 1:
+            return self.stack.pop()
+
+        item = self.stack.pop()
+        dequeued_item = self.dequeue()
+        self.stack.append(item)
+        return dequeued_item
